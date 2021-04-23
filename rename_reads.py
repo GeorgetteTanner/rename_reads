0bb2a9c9cd3518f54da3b58271f59284853fcc0f
Georgette Tanner <medgnt@leeds.ac.uk>
2021-04-23 17:33:09 +0100

    Update rename_reads.py

--- rename_reads.py
+++ rename_reads.py
@@ -8,9 +8,8 @@ Created on Sun Feb 21 20:43:03 2021
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Rename reads in fastq file as "STRING_{sequential number}"')
+parser = argparse.ArgumentParser(description='Rename reads in fastq file by adding a sequential number starting from 0 to the end of existing read names')
 parser.add_argument('-i', '--input', dest='input', help='Input fastq file')
-parser.add_argument('-s', '--string', dest='string', help='STRING to include in read names')
 parser.add_argument('-o', '--output', dest='output', help='Output fastq file')
 args = parser.parse_args()
 
@@ -23,9 +22,8 @@ with open(args.input,'r') as ifile:
         line=ifile.readline()
         while line:
 
-            l=line.split(' ')
-            l[0]='@'+args.string+'_'+str(i)
-            ofile.write(' '.join(l))
+            l=line.strip()
+            ofile.write(l+'_'+str(i)+'\n')
 
             line=ifile.readline()
             ofile.write(line)
